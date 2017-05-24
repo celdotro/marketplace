86985615f12d5534188e007d9ed8328dac29f5ca
Alin Stefan <ainzz@users.noreply.github.com>
2017-05-24 17:15:10 +0300

    added base structure

---
+++ src/Products/ProductsList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace celmarket\Products;
+
+use celmarket\Dispatcher;
+
+class ProductsList
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+
+    public function list($arg1)
+    {
+    }
+}
