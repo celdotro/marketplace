988602a67b2ab0508b01bdb1cee0d7f4804edde2
Alin Stefan <alintanase92@gmail.com>
2017-05-25 16:07:09 +0300

    added auth class

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'vendor/autoload.php';
+
+use celmarket\Auth;
+Auth::setUserDetails('mbd', 'acadele');
+$auth = Auth::getInstance();
+$token = $auth->getToken();
+var_dump($token);
