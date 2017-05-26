bf1b7672406a217e145699268f1aedfc0af62cb7
Florin D <fredtux@users.noreply.github.com>
2017-05-26 16:36:41 +0300

    Added listProducts 	modified:   src/Products/ProductsList.php

--- src/Products/ProductsList.php
+++ src/Products/ProductsList.php
@@ -25,4 +25,23 @@ class ProductsList
         return $result;
     }
 
+    public function listProducts($start, $limit, $search = null){
+        // Sanity check
+        if(!isset($start) || !is_int($start) || $start < 0) throw new \Exception('Precizati o valoare de start numar natural');
+        if(!isset($limit) || !is_int($limit) || $limit < 0) throw new \Exception('Precizati un numar natural pentru limita');
+
+        // Set method and action
+        $method = 'products';
+        $action = 'readProducts';
+
+        // Set data
+        $data = array('start' => $start, 'limit' => $limit);
+        if(!is_null($search)) $data['search'] = $search;
+
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
 }
