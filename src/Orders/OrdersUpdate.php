2d6adb348d6ea3ce5c77b3b2f2a29bcb84aa227f
Florin D <fredtux@users.noreply.github.com>
2017-06-09 17:32:50 +0300

    Removed unnecessary methods 	modified:   src/Orders/OrdersUpdate.php

--- src/Orders/OrdersUpdate.php
+++ src/Orders/OrdersUpdate.php
@@ -7,56 +7,6 @@ use celmarket\Dispatcher;
 class OrdersUpdate {
 
     /**
-     * Add products to order
-     * @param $cmd
-     * @param $arrModels
-     * @return \Psr\Http\Message\ResponseInterface
-     * @throws \Exception
-     */
-    public function addProductsToOrder($cmd, $arrModels){
-        // Sanity check
-        if(!isset($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
-        if(!isset($arrModels) || !is_array($arrModels) || empty($arrModels)) throw new \Exception('$arrModels trebuie sa fie un array care sa contina modelele produselor ce vor fi adaugate in comanda');
-
-        // Set method and action
-        $method = 'orders';
-        $action = 'AddProductsToOrder';
-
-        // Set data
-        $data = array('cmd' => $cmd, 'products' => $arrModels);
-
-        // Send request and retrieve response
-        $result = Dispatcher::send($method, $action, $data);
-
-        return $result;
-    }
-
-    /**
-     * Remove products from order
-     * @param $cmd
-     * @param $arrModels
-     * @return \Psr\Http\Message\ResponseInterface
-     * @throws \Exception
-     */
-    public function removeProductsFromOrder($cmd, $arrModels){
-        // Sanity check
-        if(!isset($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
-        if(!isset($arrModels) || !is_array($arrModels) || empty($arrModels)) throw new \Exception('$arrModels trebuie sa fie un array care sa contina modelele produselor ce vor fi eliminate din comanda');
-
-        // Set method and action
-        $method = 'orders';
-        $action = 'RemoveProductsFromOrder';
-
-        // Set data
-        $data = array('cmd' => $cmd, 'products' => $arrModels);
-
-        // Send request and retrieve response
-        $result = Dispatcher::send($method, $action, $data);
-
-        return $result;
-    }
-
-    /**
      * Update the products of an order
      * @param $cmd
      * @param $arrProducts
