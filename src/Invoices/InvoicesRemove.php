aa4783db4d667a600ffd4e7ccf08858a184fa635
Florin D <fredtux@users.noreply.github.com>
2017-06-09 13:03:59 +0300

    Added invoices remove 	modified:   src/Invoices/InvoicesRemove.php

---
+++ src/Invoices/InvoicesRemove.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace celmarket\Invoices;
+
+use celmarket\Dispatcher;
+
+class InvoicesRemove {
+
+    public function removeInvoice($cmd){
+        // Sanity check
+        if(!isset($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
+
+        // Set method and action
+        $method = 'orders';
+        $action = 'billingDelete';
+
+        // Set data
+        $data = array('cmd' => $cmd);
+
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
+}
