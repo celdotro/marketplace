1be8a30e94fbdba1b6b52773270fd1c247dde935
Florin D <fredtux@users.noreply.github.com>
2017-06-19 18:27:51 +0300

    Added updateData for products 	modified:   src/Products/ProductsUpdate.php

--- src/Products/ProductsUpdate.php
+++ src/Products/ProductsUpdate.php
@@ -9,11 +9,11 @@ class ProductsUpdate {
      * [RO] Actualizeaza un array de produse. Fiecare produs are un array de caracteristici.
      * [EN] Update an array of products. Each product has an array of characteristics.
      * @param $arrProducts
-     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Exception
      */
     public function updateStockAndPrice($arrProducts){
         // Sanity check - for older versions of PHP
-        if(!is_array($arrProducts)) throw new Exception('Functia primeste ca parametru un array cu datele fiecarui produs grupate intr-un sub-array');
+        if(!is_array($arrProducts) || empty($arrProducts)) throw new \Exception('Functia primeste ca parametru un array cu datele fiecarui produs grupate intr-un sub-array');
 
         // Set method and action
         $method = 'products';
@@ -21,6 +21,28 @@ class ProductsUpdate {
 
         // Set data
         $data = array('products' => $arrProducts);
+
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
+    /**
+     * Updates an already existing product's data
+     * @param $arrProducts
+     * @throws \Exception
+     */
+    public function updateData($arrProducts){
+        // Sanity check
+        if(!is_array($arrProducts) || empty($arrProducts)) throw new \Exception('Functia primeste ca parametru un array cu datele fiecarui produs grupate intr-un sub-array');
+
+        // Set method and action
+        $method = 'products';
+        $action = 'saveProducts';
+
+        // Set data
+        $data = array('products' => json_encode($arrProducts));
 
         // Send request and retrieve response
         $result = Dispatcher::send($method, $action, $data);
