22614f992ec712c348689e678f85c2ec6196011a
Florin D <fredtux@users.noreply.github.com>
2017-06-21 11:36:37 +0300

    Server address only in Config.php 	modified:   src/Auth.php 	modified:   src/Config.php 	modified:   src/Dispatcher.php

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,6 @@ namespace celmarket;
 
 class Config
 {
-    const MIDDLE_IP = '192.168.0.89';
-    const MIDDLE_HTTP = 'http://192.168.0.89/market_api/';
+    const API_HTTP = 'http://192.168.0.89/market_api/';
     const TOKEN_PATH = './token';
 }
