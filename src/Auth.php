c26dadd76d35de73a7b34c07e91463f4a8453da1
Florin D <fredtux@users.noreply.github.com>
2017-06-22 12:57:05 +0300

    Added comments 	modified:   src/Auth.php

--- src/Auth.php
+++ src/Auth.php
@@ -5,62 +5,110 @@ use GuzzleHttp\Client;
 use celmarket\Config;
 
 class Auth
-{
+{ // Implements Singleton pattern
+    // Singleton instance
     private static $_instance = null;
 
+    // Data necesary for authentication
     private static $username = null;
     private static $password = null;
     private static $token = null;
 
+    // Connection timeout for Guzzle
     const TIMEOUT = 30; // 30s timeout
 
+    /**
+     * Auth constructor.
+     * @param bool $regenerate
+     * @throws \Exception
+     */
     private function __construct($regenerate = false)
     {
+        // Sanity check
         if (empty(static::$username) || empty(static::$password)) {
             throw new \Exception('Username or password is missing');
         }
+
+        // Check if token exists and there is no need to regenerate
         if (file_exists(Config::TOKEN_PATH) && !$regenerate) {
+            // Read the token file and set the static attribute $token with the file content
             static::$token = file_get_contents(Config::TOKEN_PATH);
             return true;
         }
+
+        // If token doesn't exist or if regeneration is imposed, then retrieve a new token from the server using Guzzle
+        //// New Guzzle client
         $guzzleClient = new Client(array('timeout'  => self::TIMEOUT));
+        //// Send POST request to the login page
         $response = $guzzleClient->request('POST', Config::API_HTTP . 'login/actionLogin', array('form_params' => array('username' => static::$username, 'password' => static::$password)));
+        //// Retrieve response
         $body = $response->getBody()->getContents();
+        //// Decode the response and throw error if the response is not a JSON object
         $res = json_decode($body, true);
         if (json_last_error() !== 0) throw new \Exception('Eroare la parsarea raspunsului trimis de serverul de autentificare: ' . $body);
+
+        //// Process response
         try{
-            if ($res['tokenStatus']) {
-                static::$token = $res['message'];
+            if (!is_null($res['tokenStatus']) && !is_null($res['message']) && $res['tokenStatus']) { // Check if the response has a valid form and the token status is true
+                // Write the message content in the token file ($res['message'] is the token)
                 file_put_contents(Config::TOKEN_PATH, $res['message']);
+                // Set the static attribute $token with the message content
+                static::$token = $res['message'];
+            } else {
+                // Throw invalid response exception and add the received response in it's undecoded form
+                throw new \Exception('Raspuns invalid: ' . $body);
             }
         } catch (\Exception $e) {
+            // Catch all exceptions, add a message for clarification and re-throw them up the stack
             throw new \Exception('Eroare in procesul de autentificare: ' . $e->getMessage());
         }
     }
 
+    /**
+     * Retrieve singleton's instance
+     * @return Auth|null
+     */
     public static function getInstance()
     {
+        // Check if instance exists and if it doesn't exist (singleton's first use), then generate one
         if (empty(self::$_instance)) {
             self::$_instance = new Auth();
         }
         return self::$_instance;
     }
 
+    /**
+     * Regenerate authentication token
+     * @return null
+     */
     public static function regenerateToken()
     {
+        // Call constructor with forced regeneration
         self::$_instance = new Auth(true);
         return self::$token;
     }
 
+    /**
+     * Set username and password
+     * @param $username
+     * @param $password
+     * @throws \Exception
+     */
     public static function setUserDetails($username, $password)
     {
+        // Sanity check
         if(is_null($username) || empty($username)) throw new \Exception('Specificati un nume de utilizator valid');
         if(is_null($password) || empty($password)) throw new \Exception('Specificati o parola valida');
 
+        // Set attributes
         self::$username = $username;
         self::$password = $password;
     }
 
+    /**
+     * Token retrieval logic
+     * @return bool|null|string
+     */
     public function getToken()
     {
         return file_exists(Config::TOKEN_PATH) ? file_get_contents(Config::TOKEN_PATH) : static::regenerateToken();
