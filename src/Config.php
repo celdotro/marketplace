421f45f9796875b9d0598dffe797e8f9e723653b
Florin D <fredtux@users.noreply.github.com>
2017-06-23 11:11:28 +0300

    Changed config IP 	modified:   src/Config.php

--- src/Config.php
+++ src/Config.php
@@ -12,7 +12,7 @@ namespace celmarket;
 class Config
 {
     // API's address
-    const API_HTTP = 'http://192.168.0.89/market_api/';
+    const API_HTTP = 'http://192.168.0.85/market_api/';
 
     // Path to token file
     const TOKEN_PATH = './token';
