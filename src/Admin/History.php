e9a4884ac48de166a8c73b79b29fdeced37def81
Florin D <fredtux@users.noreply.github.com>
2017-07-11 18:18:37 +0300

    Fixed optional fields 	modified:   src/Admin/History.php

--- src/Admin/History.php
+++ src/Admin/History.php
@@ -9,13 +9,13 @@ class History {
     /**
      * [RO] Returneaza date despre istoricul importurilor (https://github.com/celdotro/marketplace/wiki/Istoric-importuri)
      * [EN] Returns data about import history (https://github.com/celdotro/marketplace/wiki/Import-history)
-     * @param $date_start
-     * @param $date_stop
+     * @param null $date_start
+     * @param null $date_stop
      * @param null $import_type
      * @param int $page
      * @throws \Exception
      */
-    public function getImportHistory ($date_start, $date_stop, $import_type = NULL, $page = 0) {
+    public function getImportHistory ($date_start = null, $date_stop = null, $import_type = NULL, $page = 0) {
         // Sanity check
         if (!isset($date_start) || strtotime($date_start) == false) throw new \Exception('Specificati data de inceput a importului');
         if (!isset($date_stop) || strtotime($date_stop) == false) throw new \Exception('Specificati data de sfarsit a importului');
@@ -25,7 +25,8 @@ class History {
         $action = 'getImportHistory';
 
         // Set data
-        $data = array('data_start' => $date_start, 'data_stop' => $date_stop);
+        if (!is_null($date_start)) $data['data_start'] = $date_start;
+        if (!is_null($date_stop)) $data['data_stop'] = $date_stop;
         if (!is_null($import_type)) $data['tip_import'] = $import_type;
         if (!is_null($page) && $page != 0) $data['page'] = $page;
 
