975e1ec3ab96ace60191491fbf50966e428401c8
Florin D <fredtux@users.noreply.github.com>
2017-08-03 12:44:23 +0300

    Added method for removing products from order based on their models 	modified:   src/Orders/OrdersUpdate.php

--- src/Orders/OrdersUpdate.php
+++ src/Orders/OrdersUpdate.php
@@ -58,4 +58,30 @@ class OrdersUpdate {
         return $result;
     }
 
+    /**
+     * [RO] Sterge un produs dintr-o comanda existenta (https://github.com/celdotro/marketplace/wiki/Stergerea-unui-model-de-produs-din-comanda)
+     * [EN] Remove a product model from an existing order (https://github.com/celdotro/marketplace/wiki/Remove-product-model-from-order)
+     * @param $cmd
+     * @param $arrProducts
+     * @return mixed
+     * @throws \Exception
+     */
+    public function removeProductsFromOrder($cmd, $arrProducts){
+        // Sanity check
+        if(!isset($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
+        if(!isset($arrProducts) || !is_array($arrProducts) || empty($arrProducts)) throw new \Exception('$arrProducts trebuie sa fie un array care sa contina un alt array cu datele produselor pe care doriti sa le actualizati in comanda');
+
+        // Set method and action
+        $method = 'orders';
+        $action = 'removeProductsFromOrder';
+
+        // Set data
+        $data = array('order' => $cmd, 'data' => json_encode($arrProducts));
+
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
 }
