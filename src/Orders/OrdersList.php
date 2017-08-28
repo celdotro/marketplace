3fdffca5c45cb7629e0ab7f5346e52cb76edb8b2
Florin D <fredtux@users.noreply.github.com>
2017-08-28 14:19:26 +0300

    Changed orders listing parameters

--- src/Orders/OrdersList.php
+++ src/Orders/OrdersList.php
@@ -6,18 +6,19 @@ use celmarket\Dispatcher;
 class OrdersList {
 
     /**
-     * [RO] Listeaza comenzile unui client filtrate in functie de data. Lista poate fi scurtata folosind o pozitie
-     * de start si o limita. Nu suporta mai mult de 10 inregistrari. Prima categorie va avea implicit numele "parent".
+     * [RO] Listeaza comenzile unui client. Lista este scurtata folosind o pozitie
+     * de start si o limita. Nu suporta mai mult de 50 inregistrari.
      * (https://github.com/celdotro/marketplace/wiki/Listare-comenzi)
-     * [EN] List orders for a customer filtered by date. It can be shrunk using a start position and a limit.
-     * It can hold a maximum of 10 records. The first category will be named 'parent'.
+     * [EN] List orders for a customer. It is shrunk by using a start position and a limit.
+     * It can hold a maximum of 50 records.
      * (https://github.com/celdotro/marketplace/wiki/List-orders)
      * @param $start
      * @param $limit
      * @param $options
+     * @param $status
      * @return mixed
      */
-    public function listOrders($start, $limit, $options){
+    public function listOrders($start, $limit, $options, $status = null){
         // Sanity check
         if(!isset($start) || !is_int($start)) throw new Exception('$start trebuie sa fie de tip integer');
         if(!isset($limit) || !is_int($limit)) throw new Exception('$limit trebuie sa fie de tip integer');
@@ -33,6 +34,8 @@ class OrdersList {
             'limit' =>  $limit,
             'filters'  =>  json_encode($options),
         );
+
+        if(!is_null($status)) $data['status'] = $status;
 
         // Send request and retrieve response
         $result = Dispatcher::send($method, $action, $data);
