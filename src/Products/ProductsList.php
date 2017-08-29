9edd9e3ca3ef42e2b46dad9f1ec6c95e85db7404
Florin D <fredtux@users.noreply.github.com>
2017-08-29 13:28:20 +0300

    Added filters to product listing 	modified:   src/Products/ProductsList.php

--- src/Products/ProductsList.php
+++ src/Products/ProductsList.php
@@ -27,18 +27,18 @@ class ProductsList
     }
 
     /**
-     * [RO] Listeaza produsele. Lista poate fi scurtata folosind o valoare de start si o limita. De asemenea, produsele
-     * pot fi filtrate in functie de nume. (https://github.com/celdotro/marketplace/wiki/Listeaza-produse)
-     * [EN] Lists products. The list can be shrunk using a start and limit value. Also, the products can be filtered
-     * based on their name. (https://github.com/celdotro/marketplace/wiki/List-products)
+     * [RO] Listeaza produsele. Lista este scurtata folosind o valoare de start si o limita. De asemenea, produsele
+     * pot fi filtrate. (https://github.com/celdotro/marketplace/wiki/Listeaza-produse)
+     * [EN] Lists products. The list is shrunk using a start and limit value. Also, the products can be filtered. (https://github.com/celdotro/marketplace/wiki/List-products)
      * @param $start
      * @param $limit
      * @param null $search
      * @param bool $forceCount
+     * @param null $filters
      * @return mixed
      * @throws \Exception
      */
-    public function listProducts($start, $limit, $search = null, $forceCount = false){
+    public function listProducts($start, $limit, $search = null, $forceCount = false, $filters = null){
         // Sanity check
         if(!isset($start) || !is_int($start) || $start < 0) throw new \Exception('Precizati o valoare de start numar natural');
         if(!isset($limit) || !is_int($limit) || $limit < 0) throw new \Exception('Precizati un numar natural pentru limita');
@@ -50,6 +50,7 @@ class ProductsList
         // Set data
         $data = array('start' => $start, 'limit' => $limit, 'forceCount' => $forceCount);
         if(!is_null($search)) $data['search'] = $search;
+        if(!is_null($filters)) $data['filters'] = $filters;
 
         // Send request and retrieve response
         $result = Dispatcher::send($method, $action, $data);
