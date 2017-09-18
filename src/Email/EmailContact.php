9736ec2ce0bb6624d95a781135f8fff4a71e639d
Florin D <fredtux@users.noreply.github.com>
2017-09-18 12:57:40 +0300

    1

--- src/Email/EmailContact.php
+++ src/Email/EmailContact.php
@@ -8,7 +8,7 @@ class EmailContact {
     public function sendBugReport($message, $debug){
         // Sanity check
         if(is_null($message) || empty(trim($message)) || strlen(trim($message)) < 10) throw new \Exception('Specificati un mesaj valid de cel putin 10 caractere in afara de spatiu');
-        if(empty($debug)) throw new \Exception('Specificati mesajul de debug');
+        if(empty($debug) && !is_array($debug)) throw new \Exception('Specificati mesajul de debug sub forma unui array');
 
         // Set method and action
         $method = 'email';
