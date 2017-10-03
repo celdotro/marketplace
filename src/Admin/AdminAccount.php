1293371cae4b4a6ba47bd15786cf601a41d998d0
Florin D <fredtux@users.noreply.github.com>
2017-10-03 12:17:59 +0300

    Minor changes

--- src/Admin/AdminAccount.php
+++ src/Admin/AdminAccount.php
@@ -7,15 +7,16 @@ use celmarket\Dispatcher;
 class AdminAccount {
 
     /**
-     * [RO] Actualizeaza urmatoarele date aferente afiliatului: CIF, IBAN si numar de telefon (https://github.com/celdotro/marketplace/wiki/Actualizare-informatii-cont)
-     * [EN] Updates the following data of an affiliate: CIF, IBAN, and phone number (https://github.com/celdotro/marketplace/wiki/Actualizare-informatii-cont)
+     * [RO] Actualizeaza urmatoarele date aferente afiliatului: CIF, IBAN, numar de telefon si parola (https://github.com/celdotro/marketplace/wiki/Actualizare-informatii-cont)
+     * [EN] Updates the following data of an affiliate: CIF, IBAN, phone number, and password (https://github.com/celdotro/marketplace/wiki/Actualizare-informatii-cont)
      * @param null $cif
      * @param null $iban
      * @param null $telephone
+     * @param null $password
      * @return mixed
      * @throws \Exception
      */
-    public function updateAccountInformation ($cif = NULL, $iban = NULL, $telephone = NULL) {
+    public function updateAccountInformation ($cif = NULL, $iban = NULL, $telephone = NULL, $password = NULL) {
         // Sanity check - skip it because it needs additional methods and packages which will just bloat this project
         // All proper checks for data integrity are done on our server
 
@@ -28,6 +29,7 @@ class AdminAccount {
         if(!is_null($cif)) $data['cif'] = $cif;
         if(!is_null($iban)) $data['iban'] = $iban;
         if(!is_null($telephone)) $data['telephone'] = $telephone;
+        if(!is_null($password)) $data['password'] = $password;
 
         if(empty($data)) throw new \Exception('Specificati cel putin 1 camp');
 
