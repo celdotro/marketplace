e4f6b4c4be2a9fca94956766f54f0c3503d77dbf
Florin D <fredtux@users.noreply.github.com>
2017-10-06 11:53:48 +0300

    Added products to bill

--- src/Admin/AdminReports.php
+++ src/Admin/AdminReports.php
@@ -24,4 +24,23 @@ class AdminReports {
         return $result;
     }
 
+    /**
+     * [RO] Factureaza produsele disponibile (https://github.com/celdotro/marketplace/wiki/Factureaza-produse)
+     * [EN] Bills available products (https://github.com/celdotro/marketplace/wiki/Bill-products)
+     * @param $products
+     * @return mixed
+     */
+    public function billProducts($products){
+        // Set method and action
+        $method = 'admininfo';
+        $action = 'BillProducts';
+
+        // Set data
+        $data = array('products' => $products);
+
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
 }
