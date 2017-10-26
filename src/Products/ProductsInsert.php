640cf80e25b54aab2825dfdf492963ec7d47540c
Florin D <fredtux@users.noreply.github.com>
2017-10-26 12:57:56 +0300

    Added multiple fixes

--- src/Products/ProductsInsert.php
+++ src/Products/ProductsInsert.php
@@ -76,7 +76,7 @@ class ProductsInsert {
         // Sanity check - for older versions of PHP
         if (!isset($products_model)) throw new \Exception('Specificati un model de produs');
         if (!isset($stoc)) throw new \Exception('Specificati stocul');
-        if (!isset($pret) || empty($charactValues)) throw new \Exception('Specificati pretul');
+        if (!isset($pret)) throw new \Exception('Specificati pretul');
 
         // Set method and action
         $method = 'products';
