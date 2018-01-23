6cbf9f7fd0b24d220cd6be5f9863bf2cc54729bc
Florin D <fredtux@users.noreply.github.com>
2018-01-23 11:29:00 +0200

    1

--- src/Email/EmailCommunication.php
+++ src/Email/EmailCommunication.php
@@ -36,4 +36,69 @@ class EmailCommunication {
         return $result;
     }
 
+    public function GetServiceRequests($minDate = null, $maxDate = null, $product_model = null, $start = null){
+        // Set method and action
+        $method = 'email';
+        $action = 'GetServiceRequests';
+
+        // Set data
+        $data = array('dummy' => 1);
+        if (!is_null($minDate)) $data['minDate'] = $minDate;
+        if (!is_null($maxDate)) $data['maxDate'] = $maxDate;
+        if (!is_null($product_model)) $data['product_model'] = $product_model;
+        if (!is_null($start)) $data['start'] = $start;
+
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
+    public function answerServiceRequest($id, $answer){
+        // Set method and action
+        $method = 'email';
+        $action = 'AnswerServiceRequest';
+
+        // Set data
+        $data = array(
+            'id' => $id,
+            'answer' => $answer
+        );
+
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
+    public function getReturnRequests($minDate = null, $maxDate = null, $product_model = null, $start = null){
+        // Set method and action
+        $method = 'email';
+        $action = 'GetReturnRequests';
+
+        // Set data
+        $data = array('dummy' => 1);
+        if (!is_null($minDate)) $data['minDate'] = $minDate;
+        if (!is_null($maxDate)) $data['maxDate'] = $maxDate;
+        if (!is_null($product_model)) $data['product_model'] = $product_model;
+        if (!is_null($start)) $data['start'] = $start;
+
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
+
+    public function answerReturnRequests($id, $answer){
+        // Set method and action
+        $method = 'email';
+        $action = 'AnswerReturnRequests';
+
+        // Set data
+        $data = array(
+            'id' => $id,
+            'answer' => $answer
+        );
+
+        $result = Dispatcher::send($method, $action, $data);
+
+        return $result;
+    }
 }
