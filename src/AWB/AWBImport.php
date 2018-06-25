caa93292a379214b9f3e655e9122a91d131b14b8
Florin D <fredtux@users.noreply.github.com>
2018-06-25 17:18:45 +0300

    Replaced setAwb with importAwb

--- src/AWB/AWBImport.php
+++ src/AWB/AWBImport.php
@@ -10,9 +10,10 @@ class AWBImport {
      * [RO] Seteaza un AWB pentru o comanda (https://github.com/celdotro/marketplace/wiki/Creare-AWB)
      * [EN] Add an AWB for a specific order (https://github.com/celdotro/marketplace/wiki/AWB-Import)
      * @param $cmd
-     * @param null $idAdresaRidicare
+     * @param $awb
+     * @param $idAdresaRidicare
      * @return mixed
-     * @throws \Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function importAWB($cmd, $awb, $idAdresaRidicare){
         // Sanity check
@@ -20,7 +21,7 @@ class AWBImport {
 
         // Set method and action
         $method = 'orders';
-        $action = 'setAwb';
+        $action = 'importAWB';
 
         // Set data
         $data = array('cmd' => $cmd, 'awb' => $awb, 'idAdresaRidicare' => $idAdresaRidicare);
