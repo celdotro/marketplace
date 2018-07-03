ef0c3d5f6525c623dca4c54c5b4d84a9b6580375
Florin D <fredtux@users.noreply.github.com>
2018-07-03 17:20:40 +0300

    Replaced import invoice

--- src/Invoices/InvoicesData.php
+++ src/Invoices/InvoicesData.php
@@ -31,33 +31,37 @@ class InvoicesData {
     }
 
 
-//    /**
-//     * [RO] Genereaza o noua factura (https://github.com/celdotro/marketplace/wiki/Adaugare-factura)
-//     * [EN] Generates a new invoice (https://github.com/celdotro/marketplace/wiki/Add-invoice)
-//     * @param $cmd
-//     * @param $serie
-//     * @param $nr_fact
-//     * @return mixed
-//     * @throws \Exception
-//     */
-//    public function importInvoice($cmd, $serie, $nr_fact){
-//      // Sanity check
-//      if(empty($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
-//      if(empty($serie)) throw new \Exception('Specificati seria facturii');
-//      if(empty($nr_fact)) throw new \Exception('Specificati numarul  facturii');
-//
-//      // Set method and action
-//      $method = 'orders';
-//      $action = 'importInvoice';
-//
-//      // Set data
-//      $data = array('order' => $cmd, 'data' => json_encode($arrProducts));
-//
-//      // Send request and retrieve response
-//      $result = Dispatcher::send($method, $action, $data);
-//
-//      return $result;
-//    }
+    /**
+     * [RO] Genereaza o noua factura (https://github.com/celdotro/marketplace/wiki/Adaugare-factura)
+     * [EN] Generates a new invoice (https://github.com/celdotro/marketplace/wiki/Add-invoice)
+     * @param $cmd
+     * @param $serie
+     * @param $nr_fact
+     * @return mixed
+     * @throws \Exception
+     */
+    public function importInvoice($cmd, $serie, $nr_fact){
+      // Sanity check
+      if(empty($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
+      if(empty($serie)) throw new \Exception('Specificati seria facturii');
+      if(empty($nr_fact)) throw new \Exception('Specificati numarul  facturii');
+
+      // Set method and action
+      $method = 'orders';
+      $action = 'importInvoice';
+
+      // Set data
+      $data = array(
+          'orders_id' => $cmd,
+          'serie' => $serie,
+          'nr' => $nr_fact
+      );
+
+      // Send request and retrieve response
+      $result = Dispatcher::send($method, $action, $data);
+
+      return $result;
+    }
 
 //    public function setInvoiceData($cmd, $serie, $nr){
 //        // Sanity check
