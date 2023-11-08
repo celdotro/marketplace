7c47e01e90c9587c769644614fe27bde0a9ecd31
Alin <alin@cel.ro>
2023-11-08 18:56:27 +0200

    fixed invoiceData component

--- src/Invoices/InvoicesData.php
+++ src/Invoices/InvoicesData.php
@@ -4,8 +4,8 @@ namespace celmarket\Invoices;
 
 use celmarket\Dispatcher;
 
-class InvoicesData {
-
+class InvoicesData
+{
     /**
      * DEPRECATED
      * [RO] Sterge factura unei facturi (https://github.com/celdotro/marketplace/wiki/Stergere-factura)
@@ -14,9 +14,12 @@ class InvoicesData {
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function removeInvoiceOld($ordersID){
+    public function removeInvoiceOld($ordersID)
+    {
         // Sanity check
-        if(!isset($ordersID) || !is_int($ordersID)) throw new \Exception('Specificati comanda');
+        if(!isset($ordersID) || !is_int($ordersID)) {
+            throw new \Exception('Specificati comanda');
+        }
 
         // Set method and action
         $method = 'orders';
@@ -31,7 +34,7 @@ class InvoicesData {
         return $result;
     }
 
-      /**
+    /**
      * [RO] Sterge factura unei facturi (https://github.com/celdotro/marketplace/wiki/Stergere-factura)
      * [EN] Deletes an order's invoice (https://github.com/celdotro/marketplace/wiki/Remove-Invoice)
      * @param $ordersID
@@ -40,11 +43,18 @@ class InvoicesData {
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function removeInvoice($ordersID, $series, $number){
+    public function removeInvoice($ordersID, $series, $number)
+    {
         // Sanity check
-        if(!isset($ordersID) || !is_int($ordersID)) throw new \Exception('Specificati comanda');
-        if(!isset($series)) throw new \Exception('Specificati seria facturii');
-        if(!isset($number)) throw new \Exception('Specificati numarul facturii');
+        if(!isset($ordersID) || !is_int($ordersID)) {
+            throw new \Exception('Specificati comanda');
+        }
+        if(!isset($series)) {
+            throw new \Exception('Specificati seria facturii');
+        }
+        if(!isset($number)) {
+            throw new \Exception('Specificati numarul facturii');
+        }
 
         // Set method and action
         $method = 'orders';
@@ -59,7 +69,6 @@ class InvoicesData {
         return $result;
     }
 
-
     /**
      * DEPRECATED
      * [RO] Genereaza o noua factura (https://github.com/celdotro/marketplace/wiki/Adaugare-factura)
@@ -70,99 +79,116 @@ class InvoicesData {
      * @return mixed
      * @throws \Exception
      */
-    public function importInvoiceOld($cmd, $serie, $nr_fact){
-      // Sanity check
-      if(empty($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
-      if(empty($serie)) throw new \Exception('Specificati seria facturii');
-      if(empty($nr_fact)) throw new \Exception('Specificati numarul  facturii');
+    public function importInvoiceOld($cmd, $serie, $nr_fact)
+    {
+        // Sanity check
+        if(empty($cmd) || !is_int($cmd)) {
+            throw new \Exception('Specificati comanda');
+        }
+        if(empty($serie)) {
+            throw new \Exception('Specificati seria facturii');
+        }
+        if(empty($nr_fact)) {
+            throw new \Exception('Specificati numarul  facturii');
+        }
 
-      // Set method and action
-      $method = 'orders';
-      $action = 'importInvoice';
+        // Set method and action
+        $method = 'orders';
+        $action = 'importInvoice';
 
-      // Set data
-      $data = array(
-          'orders_id' => $cmd,
-          'serie' => $serie,
-          'nr_fact' => $nr_fact
-      );
+        // Set data
+        $data = array(
+            'orders_id' => $cmd,
+            'serie' => $serie,
+            'nr_fact' => $nr_fact
+        );
 
-      // Send request and retrieve response
-      $result = Dispatcher::send($method, $action, $data);
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
 
-      return $result;
+        return $result;
     }
 
-     /**
-     * [RO] Ataseaza o factura (https://github.com/celdotro/marketplace/wiki/Adaugare-factura)
-     * [EN] Attach an invoice (https://github.com/celdotro/marketplace/wiki/Add-invoice)
-     * @param $cmd
-     * @param $serie
-     * @param $nr_fact
-     * @return mixed
-     * @throws \Exception
-     */
-    public function importInvoice($cmd, $series = '', $number = '', $providerId = 0, $file = null){
-      // Sanity check
-      if(empty($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
+    /**
+    * [RO] Ataseaza o factura (https://github.com/celdotro/marketplace/wiki/Adaugare-factura)
+    * [EN] Attach an invoice (https://github.com/celdotro/marketplace/wiki/Add-invoice)
+    * @param $cmd
+    * @param $serie
+    * @param $nr_fact
+    * @return mixed
+    * @throws \Exception
+    */
+    public function importInvoice($cmd, $series = '', $number = '', $providerId = 0, $file = null)
+    {
+        // Sanity check
+        if(empty($cmd) || !is_int($cmd)) {
+            throw new \Exception('Specificati comanda');
+        }
 
-      if(empty($providerId)) {
-          if(empty($series)) {
-              throw new \Exception('Specificati seria facturii');
-          }
-          if(empty($number)) {
-            throw new \Exception('Specificati numarul facturii');
-          }
-          if(empty(file)) {
-            throw new \Exception('Specificati calea catre fisierul cu factura');
-          }
-      }
+        if(empty($providerId)) {
+            if(empty($series)) {
+                throw new \Exception('Specificati seria facturii');
+            }
+            if(empty($number)) {
+                throw new \Exception('Specificati numarul facturii');
+            }
+            if(empty($file)) {
+                throw new \Exception('Specificati calea catre fisierul cu factura');
+            }
+            if(!file_exists($file)) {
+                throw new \Exception('Fisierul nu exista');
+            }
+        }
 
+        // Set method and action
+        $method = 'orders';
+        $action = 'addOrderInvoice';
 
-      // Set method and action
-      $method = 'orders';
-      $action = 'addOrderInvoice';
+        // Set data
+        $data = array(
+            'oid' => $cmd,
+            'series' => $series,
+            'number' => $number,
+            'providerId' => $providerId,
+        );
 
-      // Set data
-      $data = array(
-          'oid' => $cmd,
-          'series' => series,
-          'number' => $number,
-          'providerId' => $providerId,
-          'file' => $file
-      );
+        $files = [
+          'file' =>  file_get_contents($file)
+        ];
 
-      // Send request and retrieve response
-      $result = Dispatcher::send($method, $action, $data);
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data, $files);
 
-      return $result;
+        return $result;
     }
 
-      /**
+    /**
      * [RO] Listeaza facturile atasate unei comenzi (https://github.com/celdotro/marketplace/wiki/Listeaza-facturi)
      * [EN] List attached invoices for an order (https://github.com/celdotro/marketplace/wiki/List-invoices)
      * @param $cmd
      * @return mixed
      * @throws \Exception
      */
-    public function getOrderInvoices($cmd){
-      // Sanity check
-      if(empty($cmd) || !is_int($cmd)) throw new \Exception('Specificati comanda');
+    public function getOrderInvoices($cmd)
+    {
+        // Sanity check
+        if(empty($cmd) || !is_int($cmd)) {
+            throw new \Exception('Specificati comanda');
+        }
 
+        // Set method and action
+        $method = 'orders';
+        $action = 'getOrderInvoices';
 
-      // Set method and action
-      $method = 'orders';
-      $action = 'getOrderInvoices';
+        // Set data
+        $data = array(
+            'orders_id' => $cmd,
+        );
 
-      // Set data
-      $data = array(
-          'orders_id' => $cmd,
-      );
+        // Send request and retrieve response
+        $result = Dispatcher::send($method, $action, $data);
 
-      // Send request and retrieve response
-      $result = Dispatcher::send($method, $action, $data);
-
-      return $result;
+        return $result;
     }
-    
+
 }
